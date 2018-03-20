305b15b0e37a60c7b2ec5614c29658bcbb67cb46
OleksandrUtkin <32433604+OleksandrUtkin@users.noreply.github.com>
2018-03-20 23:34:25 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	$(".menu_button").click(function() {
+		$(".menu ul").slideToggle();
+	});
+
+		//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+
